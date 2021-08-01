efa985379ee73faae2c306b6c3c9e708e806438f
4L3xD <alessandra.olea@gmail.com>
2021-08-01 14:59:01 -0300

    add join(map)

--- estruturas-de-repeticao/0001_arrays.py
+++ estruturas-de-repeticao/0001_arrays.py
@@ -18,27 +18,30 @@ print('ITERAÇÃO dos elementos do array por meio da Estrutura de Repetição FO
 baaa = [ letra for letra in bananan if letra != bananan[-1]]
 print('\nITERAÇÃO do FOR com Estrutura Condicional IF: \n' + str(baaa))
 
-print('Nós concatenamos os elementos de índice [1, 0, 0, 0] do array → {}: {}.'.format(lista, bananan))
+print('\nNós concatenamos os elementos de índice [1, 0, 0, 0] do array → {}: {}.'.format(lista, bananan))
 # junção dos elementos do array
 str_bananan = ''.join(bananan)
 banana = str_bananan[1:-1]
-print("""Após realizar a junção dos elementos do array,
+print("""\nApós realizar a junção dos elementos do array,
     podemos delimitar os parâmetros de INÍCIO e FIM com os índices [1:-1] e obtemos: \'{}\'.""".format(banana))
 
+aeiou = '-'.join(map(str, banana))
+print('\nVamos juntar os elementos do array e adicionar hífens entre eles, que agora são elementos da string:\n' + aeiou)
 
 
-#for elemento in bananan:
-#    #print(elemento)
-#    #if elemento  
-#    banana = ''.join(bananan)
-#print(banana)
+print('\nTambém podemos coletar os elementos formando sílabas:')
+be_a_ba = ''.join(map(str, banana[::3]))
+ba = be_a_ba
+print(ba)
 
+be_a_ba = ''.join(map(str, banana[2::3]))
+na = be_a_ba
+print(na)
 
-#print(banana)
+be_a_ba = ''.join(map(str, banana[4::]))
+na_ = be_a_ba
+print(na_)
 
+print('\nE novamente formar a palavra:')
+print(ba + na + na)
 
-#print(tupla[-1])
-
-
-#ban = bananan.split('_', -1)
-#print(ban)
